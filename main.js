61b0bee3345ed1a0bacbf367881411d07cce81e6
larina <funkytimebruno@gmail.com>
2021-05-30 20:19:01 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var temperature;
+
+while (isNaN(Number(temperature))==true)
+ {
+  
+    temperature= prompt("Введите градусы по Цельсию");
+   if (isNaN(Number(temperature))==false) 
+   break; 
+   alert("Пожалуйста введите число");
+
+}
+
+
+
+alert( temperature+ " По Цельсию = " + convertFt(temperature)+ " по Фарингейту");
+
+console.log("тест");
+
+function convertFt(celsius)
+{
+    return (9/5)*celsius+32;
+}
+
+
+var name, admin;
+name="Василий";
+admin=name;
+alert(admin);
